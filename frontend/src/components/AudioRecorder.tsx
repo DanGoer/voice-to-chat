e30f4e27e868511cd845fbf218df16335f0e33e4
DanGoer <87978392+DanGoer@users.noreply.github.com>
2024-02-05 15:02:23 +0100

    changed minor stuff

--- frontend/src/components/AudioRecorder.tsx
+++ frontend/src/components/AudioRecorder.tsx
@@ -1,63 +1,18 @@
-import { useState, useRef } from "react";
 import { getSpeechToText } from "../utils/getSpeechToText";
+import useAudioRecorder from "../hooks/useAudioRecorder";
 
 const AudioRecorder = () => {
-  const [permission, setPermission] = useState<boolean>(false);
-  const [stream, setStream] = useState<null | MediaStream>(null);
-  const mediaRecorder = useRef(null);
-  const [recordingStatus, setRecordingStatus] = useState<string>("inactive");
-  const [audioChunks, setAudioChunks] = useState<any[]>([]);
-  const [audio, setAudio] = useState<null | string>(null);
-  const [text, setText] = useState<string>("");
-
-  const getMicrophonePermission = async () => {
-    if ("MediaRecorder" in window) {
-      try {
-        const streamData: any = await navigator.mediaDevices.getUserMedia({
-          audio: true,
-          video: false,
-        });
-        setPermission(true);
-        setStream(streamData);
-      } catch (err) {
-        alert(err.message);
-      }
-    } else {
-      alert("The MediaRecorder API is not supported in your browser.");
-    }
-  };
-
-  const startRecording = async () => {
-    setRecordingStatus("recording");
-    //create new Media recorder instance using the stream
-
-    // @ts-expect-error aaa
-    const media = new MediaRecorder(stream, { type: mimeType });
-    //set the MediaRecorder instance to the mediaRecorder ref
-    mediaRecorder.current = media;
-    //invokes the start method to start the recording process
-    mediaRecorder.current.start();
-    const localAudioChunks = [];
-    mediaRecorder.current.ondataavailable = (event) => {
-      if (typeof event.data === "undefined") return;
-      if (event.data.size === 0) return;
-      localAudioChunks.push(event.data);
-    };
-    setAudioChunks(localAudioChunks);
-  };
-
-  const stopRecording = () => {
-    setRecordingStatus("inactive");
-    //stops the recording instance
-    mediaRecorder.current.stop();
-    mediaRecorder.current.onstop = () => {
-      //creates a blob file from the audiochunks data
-      const audioBlob = new Blob(audioChunks, { type: "audio/wav" });
-      //creates a playable URL from the blob file.
-      const audioUrl = URL.createObjectURL(audioBlob);
-      setAudio(audioUrl);
-    };
-  };
+  const {
+    getMicrophonePermission,
+    startRecording,
+    stopRecording,
+    recordingStatus,
+    permission,
+    audio,
+    text,
+    setText,
+    audioChunks,
+  } = useAudioRecorder();
 
   return (
     <div>
