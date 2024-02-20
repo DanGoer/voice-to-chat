1846fc86cc239d44efa53c6b9d60f2f53f0ccb61
DanGoer <87978392+DanGoer@users.noreply.github.com>
2024-02-20 13:37:43 +0100

    added darkmode for Chat and settingsmodal

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -1,16 +1,31 @@
+import { Button } from "react-bootstrap";
 import AudioRecorder from "../components/AudioRecorder";
 import { useAuth } from "../context/AuthProvider";
+import { useState } from "react";
+import SettingsModal from "../components/SettingsModal";
 
 function Chat() {
+  const [modalShow, setModalShow] = useState<boolean>(false);
+  const [darkMode, setDarkMode] = useState<string>("light");
+  const [settings, setSettings] = useState<string>("");
   const { signOut } = useAuth();
 
   return (
-    <div>
+    <div data-bs-theme={darkMode}>
       <h1>React Media Recorder</h1>
       <div>
         <AudioRecorder />
         <button onClick={signOut}>logout</button>
       </div>
+      <Button variant="primary" onClick={() => setModalShow(true)}>
+        Settings
+      </Button>
+      <Button onClick={() => setDarkMode("dark")}>darkmode on</Button>
+      <SettingsModal
+        setSettings={setSettings}
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
     </div>
   );
 }
