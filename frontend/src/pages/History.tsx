c8dc0c37797187ac1340fed2b449e6e88d56d41d
DanGoer <87978392+DanGoer@users.noreply.github.com>
2024-02-23 11:27:41 +0100

    added history and reworked style to reusable

--- frontend/src/pages/History.tsx
+++ frontend/src/pages/History.tsx
@@ -1,5 +1,17 @@
+import { useSetup } from "../context/SetupProvider";
+
 function History() {
-  return <div>History</div>;
+  const { sideIsOpen } = useSetup();
+
+  return (
+    <section
+      className={`${
+        sideIsOpen ? "side-is-open" : ""
+      } chat-section w-100 vh-75 `}
+    >
+      history
+    </section>
+  );
 }
 
 export default History;
