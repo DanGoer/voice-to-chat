21de1214719d1111aade0127dca4071a680100e8
DanGoer <87978392+DanGoer@users.noreply.github.com>
2024-02-26 15:12:55 +0100

    splitted faq into 2 accordions and added footer and h2

--- frontend/src/pages/Faq.tsx
+++ frontend/src/pages/Faq.tsx
@@ -1,6 +1,7 @@
 import Accordion from "react-bootstrap/Accordion";
+import Footer from "../components/Footer";
 
-const accordionContent = [
+const aboutContent = [
   {
     id: "0",
     title: "test1",
@@ -15,18 +16,57 @@ const accordionContent = [
   },
 ];
 
+const howToContent = [
+  {
+    id: "0",
+    title: "test3",
+    content:
+      "53j3n2 3j52j532 j532j532 j532j532 j532j532j532j532 j532  j532 j325j 52 j532j532j 532j 532 j532j53j5",
+  },
+  {
+    id: "1",
+    title: "test4",
+    content:
+      "53j3n2 3j52j532 j532j532 j532j532 j532j532j532j532 j532  j532 j325j 52 j532j532j 532j 532 j532j53j553j3n2 3j52j532 j532j532 j532j532 j532j532j532j532 j532  j532 j325j 52 j532j532j 532j 532 j532j53j5",
+  },
+];
+
 function Faq() {
   return (
-    <Accordion defaultActiveKey="0" flush>
-      {accordionContent.map((accordionItem) => {
-        return (
-          <Accordion.Item key={accordionItem.title} eventKey={accordionItem.id}>
-            <Accordion.Header>{accordionItem.title}</Accordion.Header>
-            <Accordion.Body>{accordionItem.content}</Accordion.Body>
-          </Accordion.Item>
-        );
-      })}
-    </Accordion>
+    <>
+      <h2>FAQ</h2>
+      <section>
+        <Accordion defaultActiveKey="0" flush>
+          {aboutContent.map((accordionItem) => {
+            return (
+              <Accordion.Item
+                key={accordionItem.title}
+                eventKey={accordionItem.id}
+              >
+                <Accordion.Header>{accordionItem.title}</Accordion.Header>
+                <Accordion.Body>{accordionItem.content}</Accordion.Body>
+              </Accordion.Item>
+            );
+          })}
+        </Accordion>
+      </section>
+      <section>
+        <Accordion defaultActiveKey="0" flush>
+          {howToContent.map((accordionItem) => {
+            return (
+              <Accordion.Item
+                key={accordionItem.title}
+                eventKey={accordionItem.id}
+              >
+                <Accordion.Header>{accordionItem.title}</Accordion.Header>
+                <Accordion.Body>{accordionItem.content}</Accordion.Body>
+              </Accordion.Item>
+            );
+          })}
+        </Accordion>
+      </section>
+      <Footer />
+    </>
   );
 }
 
